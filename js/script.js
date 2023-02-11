7ad7668afbbb551079ce1650eda5f1ab14908747
lucas nakagaky <lucasnakagaki@gmail.com>
2023-02-11 12:27:03 -0300

    Efeitos com css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.sr = ScrollReveal({
+  reset: true,
+});
+
+ /* Home*/
+sr.reveal(".home-img-css", { origin: "top", distance: "80px", duration: 3000 });
+sr.reveal(".largura", { origin: "top", distance: "80px", duration: 3000 });
+sr.reveal(".social", { display: 500, origin: "top", distance: "80px", duration: 3500  });
+/* Front-End */
+sr.reveal(".icon", { origin: "right", distance: "80px", duration: 4000 });
+sr.reveal("p", { origin: "right", distance: "80px", duration: 3000 });
+/* Desing grafico */
+sr.reveal("h1", { origin: "top", distance: "80px", duration: 4000 });
+sr.reveal("h2", { origin: "left", distance: "80px", duration: 4000 });
+sr.reveal(".img_designer", { origin: "right", distance: "80px", duration: 4000 });
+/* Rodape */
+sr.reveal(".rodape_social", { display: 500, origin: "top", distance: "80px", duration: 5000 });
+
