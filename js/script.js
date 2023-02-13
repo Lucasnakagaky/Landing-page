e32c16a61be78cd77161515298fc2063c26ece48
lucas nakagaky <lucasnakagaki@gmail.com>
2023-02-13 20:11:48 -0300

    resposividade do cabecalho

--- js/script.js
+++ js/script.js
@@ -16,3 +16,16 @@ sr.reveal(".img_designer", { origin: "right", distance: "80px", duration: 4000 }
 /* Rodape */
 sr.reveal(".rodape_social", { display: 500, origin: "top", distance: "80px", duration: 5000 });
 
+
+
+/* Menu Mobile */
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+  }
+}
